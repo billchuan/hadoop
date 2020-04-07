b9152473e9c3b98c1152cf694836f23b1edfe48b
billchuan <1767618766@qq.com>
2020-04-07 11:44:25 +0800

    4月7日Hadoop更新

--- src/main/mr/WordCountDriver.java
+++ src/main/mr/WordCountDriver.java
@@ -1,7 +1,8 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
