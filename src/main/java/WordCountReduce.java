62743a0ad5d1950fbf060597103346bb57bd7e2c
billchuan <1767618766@qq.com>
2020-04-07 09:09:20 +0800

    4月7日Hadoop更新

--- src/main/java/WordCountReduce.java
+++ src/main/java/WordCountReduce.java
@@ -8,15 +8,21 @@ import java.io.IOException;
  *
  *KEYOUT,VALUE:是reduce输出的数据类型<单词，求和值>==><Text,IntWritable>
  *
- *
- *
- *
- *
  */
 public class WordCountReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
 
     @Override
     protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        //key:map的key    values:map的value
+        int sum = 0;
+        //循环遍历数据
+        for (IntWritable value : values) {
+            //获取每一个数据
+            int count = value.get();
+            //对每一个数据进行求和运算
+            sum += count;
+        }
+        context.write(key, new IntWritable(sum));
 
     }
 }
