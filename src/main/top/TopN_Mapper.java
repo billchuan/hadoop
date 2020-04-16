f41b849a18787beeefcc49bab1ae4c096b929586
billchuan <1767618766@qq.com>
2020-04-16 09:41:22 +0800

    4月16日更新

--- src/main/top/TopN_Mapper.java
+++ src/main/top/TopN_Mapper.java
@@ -1,5 +1,6 @@
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
@@ -7,22 +8,34 @@ import java.io.IOException;
 
 
 /*
-*
-*数据的输入：LongWritable,Text
-* 1 2 990 4 5 6 7
-* 12 31 32 33 43 90
-* 12 311 322 323 43 90 12 31 32 33 43 90
-* 将这些数据直接传送出去，而不进行任何处理，并将全部数据发送给reduce
-* value:IntWritable key:NullWritable
-* [Null,1 2 990 4 5 6 7 12 31 32 33 43 90 12 311 322 323 43 90 12 31 32 33 43 90]
-*
-*
-*/
-public class TopN_Mapper extends Mapper<LongWritable, Text,Text, IntWritable> {
+ *
+ *数据的输入：LongWritable,Text
+ * 1 2 990 4 5 6 7
+ * 12 31 32 33 43 90
+ * 12 311 322 323 43 90 12 31 32 33 43 90
+ * 将这些数据直接传送出去，而不进行任何处理，并将全部数据发送给reduce
+ * value:IntWritable key:NullWritable
+ * [Null,1 2 990 4 5 6 7 12 31 32 33 43 90 12 311 322 323 43 90 12 31 32 33 43 90]
+ *
+ *
+ */
+public class TopN_Mapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-
+        //读取一行数据
+        String str = value.toString();
+        //分隔数据
+        String[] counts = str.split(" ");
+        int i=0;
+        //提取每一个数字
+        for(String count:counts){
+            //字符串不能比较大小，要想比较大小，只能是数字
+            //将字符串转换成整形的数字
+            i=Integer.parseInt(count);
+            //发送数据
+            context.write(NullWritable.get(),new IntWritable(i));
+        }
 
     }
 }
